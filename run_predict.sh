c88caf8d42a630509cd3e3c38ba07754357bddf8
i05nagai <i05nagai@gmail.com>
2017-11-20 13:42:37 +0900

    Update

--- run_predict.sh
+++ run_predict.sh
@@ -4,5 +4,5 @@ path_to_this_dir=$(cd $(dirname ${0});pwd)
 python \
   -m misc.keras.cli \
   --model resnet50 \
-  --predict "${ptah_to_this_dir}/misc/keras/image/test/s_0n5d.jpg" "${ptah_to_this_dir}/misc/keras/image/test/s_003o.jpg"
+  --predict "${path_to_this_dir}/misc/keras/image/test/s_0n5d.jpg" "${path_to_this_dir}/misc/keras/image/test/s_003o.jpg"
 
