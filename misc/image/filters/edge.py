eed6ee33b6d4ac8efbbf11734bcfcc945d734bc4
i05nagai <i05nagai@gmail.com>
2017-12-05 21:11:11 +0900

    Update

--- misc/image/filters/edge.py
+++ misc/image/filters/edge.py
@@ -1,6 +1,7 @@
 import skimage
 import skimage.filters
 from .. import util
+import numpy as np
 
 
 def sharpen(image, radius=0, percent=100, threshold=4.0):
@@ -22,7 +23,7 @@ def sharpen(image, radius=0, percent=100, threshold=4.0):
     return util.to_valid_image(sharp)
 
 
-def smoothe(image, radius=0, threshold=4.0):
+def smoothe(image, radius=0, threshold=4.0, mask=None):
     """smooth
 
     :param image:
@@ -31,6 +32,36 @@ def smoothe(image, radius=0, threshold=4.0):
     """
     # Convert to float so that negatives don't cause problems
     image = skimage.img_as_float(image)
+    if mask is not None:
+        blurred = image[mask]
+    else:
+        blurred = image
     blurred = skimage.filters.gaussian(
-        image, radius, multichannel=True, truncate=threshold)
-    return util.to_valid_image(blurred * 255.0)
+        blurred, radius, multichannel=True, truncate=threshold)
+    image[mask] = blurred
+    return util.to_valid_image(image * 255.0)
+
+
+def _mask_circle(img, radius=None, center=None, inverse=False):
+    w = img.shape[1]
+    h = img.shape[0]
+    n = min(w, h)
+
+    if center is None:
+        center = (int(h / 2), int(w / 2))
+    if radius is None:
+        radius = n / 2
+
+    y, x = np.ogrid[-center[0]:h - center[0], -center[1]:w - center[1]]
+    mask = x * x + y * y <= radius * radius
+
+    if inverse:
+        mask = np.logical_not(mask)
+    return mask
+
+
+def _get_radius(img, shrink=1.0):
+    w = img.shape[1]
+    h = img.shape[0]
+    radius = min(w, h) * shrink
+    return radius
