959e954f295a7660931345e5fa1393c56d2239fb
i05nagai <i05nagai@gmail.com>
2017-12-19 20:49:04 +0900

    Update keras/backend_config.py

--- misc/keras/backend_config.py
+++ misc/keras/backend_config.py
@@ -1,15 +1,17 @@
 
 
 def set_tensorflow_config(
+        allow_growth=True,
         num_gpu=1,
         per_process_gpu_memory_fraction=1.0):
         import tensorflow as tf
         from keras.backend.tensorflow_backend import set_session
 
-        config = tf.ConfigProto()
-        (config
-         .gpu_options
-         .per_process_gpu_memory_fraction) = per_process_gpu_memory_fraction
+        gpu_options = tf.GPUOptions(allow_growth=allow_growth)
+        gpu_options.per_process_gpu_memory_fraction = per_process_gpu_memory_fraction
         visible_device_list = ",".join(str(i) for i in range(num_gpu))
-        config.gpu_options.visible_device_list = visible_device_list
-        set_session(tf.Session(config=config))
+        gpu_options.visible_device_list = visible_device_list
+
+        config = tf.ConfigProto(gpu_options=gpu_options)
+        session = tf.Session(config=config)
+        set_session(session)
