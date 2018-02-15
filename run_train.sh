f9b6da5bdc1e42ec7bf4cf0f210e35cc5ef114e4
i05nagai <i05nagai@gmail.com>
2018-02-15 14:08:34 +0900

    Update

--- run_train.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-path_to_this_dir=$(cd $(dirname ${0});pwd)
-python -m misc.keras.cli \
-  train_old \
-  --fine_tune \
-  --model_name resnet50 \
-  --data_dir "${path_to_this_dir}/misc/keras/image"
